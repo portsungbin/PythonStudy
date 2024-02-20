3b76fdc5b197a8344234d661143b904d34ba2fbe
portsungbin <sd61224@gmail.com>
2024-02-21 08:54:31 +0900

    2번 퀴즈전까지

---
+++ chapter2.py
@@ -0,0 +1,12 @@
+from random import *
+
+print(random())
+print(random() * 10) # 0.0 ~ 1.0까지 랜덤
+print(int(random() * 10)) #0~10까지 랜덤
+print(int(random() * 10)) #0~10까지 랜덤
+print(int(random() * 10)) #0~10까지 랜덤
+print(int(random() * 10) + 1) #1~10까지 랜덤
+
+print(randrange(1, 46)) # 1~45 미만의 랜덤
+
+print(randint(1, 45)) # 1~45이하의 랜덤
