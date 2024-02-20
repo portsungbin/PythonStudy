4d3a82ab0d5b4df6198347e62009560421f5094c
portsungbin <sd61224@gmail.com>
2024-02-21 08:55:31 +0900

    퀴즈1번전까지

---
+++ chapter1.py
@@ -0,0 +1,12 @@
+# 애완동물을 소개해주세요
+animal = "강이지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+''' 이렇게 하면 여러문장이 주석처리,  ctrl + 슬러시 = 모든문장 주석처리'''
+
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
