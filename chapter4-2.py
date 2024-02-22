1cc606252aa5cdaf5281d33db5dffb9595688301
portsungbin <sd61224@gmail.com>
2024-02-22 11:50:46 +0900

    퀴즈4전 튜플, 집합, 자료구조의변경

--- chapter4-2.py
+++ chapter4-2.py
@@ -42,3 +42,20 @@ print(python)
 #java를 잊어버렸음
 java.remove("김태호")
 print(java)
+
+
+
+#자료구조의 변경
+
+#커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
