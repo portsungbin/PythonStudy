28f314559f2613e51d5a707de52a0bb0bda3dbbf
portsungbin <sd61224@gmail.com>
2024-03-07 18:52:25 +0900

    퀴즈 7번문제

---
+++ Quiz7.py
@@ -0,0 +1,6 @@
+# for i in rnage(1, 51):
+#     with open(str(i) + "주차.txt", "w", encoding="utf8") as report:
+#         report.write("{0} 주차 주간보고".format(i))
+#         report.write("\n부서 : ")
+#         report.write("\n이름 : ")
+#         report.write("\n업무 요약 : ")
