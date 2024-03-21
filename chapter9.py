4e4cee6682ffa5d7e08837143d9f8e48c52dc9f2
portsungbin <sd61224@gmail.com>
2024-03-21 20:01:55 +0900

    예외처리, 에러에 관한것들

--- chapter9.py
+++ chapter9.py
@@ -19,10 +19,10 @@
 #@@@@@@@@@@@@ 에러 발생시키기, 사용자 정의 예외처리, finally @@@@@@@@@@@@@@@@@
 #에러로 인해 없는파일을 연다던지 문제를 발생했을때 강제종료를 막음으로서 완성도를 높임
 class bignumberError(Exception):
-    def __init__(self, msg):
+    def __init__(self, msg): #클래스 안의 필수존재
         self.msg = msg
 
-    def __str__(self):
+    def __repr__(self): 
         return self.msg
 
 try:
@@ -32,8 +32,9 @@ try:
     if num1 >= 10 or num2 >= 10:
         raise bignumberError("입력값 : {0} : {1}".format(num1, num2))
     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-except ValueError:
+except ValueError as err2: #내장에러
     print("잘못된 값을 입력하였습니다. 한자리 숫자만 입력하세요")
+    print(err2)
 except bignumberError as err:
     print("에러가 발생하였습니다 한자리 숫자만 입력하세요.")
     print(err)
